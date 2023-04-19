d17d1b65e6e554c65777bfd296db98196fd58212
Hossein <104124540+HosseinSayyedMousavi@users.noreply.github.com>
2023-04-19 09:48:48 +0430

    change folders

--- thentiacload/SeleniumThentia.py
+++ thentiacload/SeleniumThentia.py
@@ -1,28 +1,20 @@
 import sqlite3
-from selenium.webdriver import ChromeOptions as Options
 from selenium import webdriver
 import re
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import win32gui
 import json
-import time
-def enumWindowFunc(hwnd, windowList):
-    """ win32gui.EnumWindows() callback """
-    text = win32gui.GetWindowText(hwnd)
-    #className = win32gui.GetClassName(hwnd)
-    #print hwnd, text, className
-    if  text.find('chromedriver.exe') != -1:
-        windowList.append(hwnd)
+from selenium.webdriver import FirefoxOptions                                                                                                                                                   
+opts = FirefoxOptions()                                                                             
+opts.add_argument("--headless")                                                                     
 
 
-
-browser = webdriver.Chrome('chromedriver.exe')
 con = sqlite3.connect('thentiacloudFinal.db')
 con2=sqlite3.connect('thentiacloudFinal2.db')
 cure2=con2.cursor()
 
 def UpdateDatabase(table):
+    browser = webdriver.Firefox(options=opts)
     profiles=con.execute('select data from '+table).fetchall()
 
     try:
@@ -33,8 +25,10 @@ def UpdateDatabase(table):
     i=0
     for data in profiles:
         data = json.loads(data[0])
+        Latitude = ""
+        Longitude = ""
         try:
-            browser.get("https://cvo.ca.thentiacloud.net/webs/cvo/register/#/corporation/5d07ae985420261c56a97ff4")
+            browser.get(data["Page URL"])
             WebDriverWait(browser, 5).until(EC.visibility_of_element_located(("css selector", ".cvo-profile")))
             browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
             WebDriverWait(browser, 5).until(EC.visibility_of_element_located(("tag name", "iframe")))
@@ -44,7 +38,6 @@ def UpdateDatabase(table):
             Coordinates = re.findall(r"ll=([\d\.,-]*)",link)[0]
             Latitude = Coordinates.split(",")[0]
             Longitude = Coordinates.split(",")[1]
-            print(Latitude,Longitude)
             data["Location"]["Latitude"] = Latitude
             data["Location"]["Longitude"] = Longitude
         except:
@@ -54,5 +47,9 @@ def UpdateDatabase(table):
             con2.commit()
             i+=1
             print(table , ":",i)
+            print(Latitude,Longitude)
 
+
+# UpdateDatabase("Veterinarians")
+# UpdateDatabase("Veterinary_Practices")
 UpdateDatabase("Professional_Corporations")
